21622fd4a2e3dfef44243b048e76dc42482e40d2
Ketna20 <55664056+Ketna20@users.noreply.github.com>
2020-07-05 22:55:33 -0700

    Edit item feature is added.

--- src/main/java/spring/mvc/controller/TodoItemController.java
+++ src/main/java/spring/mvc/controller/TodoItemController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.w3c.dom.Attr;
 import spring.mvc.model.TodoItem;
 import spring.mvc.model.TodoData;
@@ -44,8 +45,13 @@ public class TodoItemController {
     }
 
     @GetMapping(Mappings.ADD_ITEM)
-    public String addEditItem(Model model){
-        TodoItem todoItem = new TodoItem("", "", LocalDate.now());
+    public String addEditItem(@RequestParam(required=false, defaultValue = "-1") int id, Model model){
+        log.info("editing id = {} ", id);
+        TodoItem todoItem = todoItemService.getItem(id);
+
+        if(todoItem == null) {
+            todoItem = new TodoItem("", "", LocalDate.now());
+        }
         model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
         return ViewNames.ADD_ITEM;
     }
@@ -53,7 +59,21 @@ public class TodoItemController {
     @PostMapping(Mappings.ADD_ITEM)
     public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem) {
         log.info("todoItem from = {} ", todoItem);
+
+        if (todoItem.getId() == 0) {
+            todoItemService.addItem(todoItem);
+        } else {
+            todoItemService.updateItem(todoItem);
+        }
+
         todoItemService.addItem(todoItem);
+        return "redirect:/" + Mappings.ITEMS;
+    }
+
+    @GetMapping(Mappings.DELETE_ITEM)
+    public String deleteItem(@RequestParam int id) {
+        log.info("Deleting item with id= {} ", id);
+        todoItemService.removeItem(id);
         return "redirect:/" + Mappings.ITEMS;
     }
 }
